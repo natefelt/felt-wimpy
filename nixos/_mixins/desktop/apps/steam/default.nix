be137170406628d35eb462ecaa2e628aa45b8c75
Nate Felt <nate@neetfelt.com>
2026-08-08 16:19:59 +0000

    feat(steam): add additional gaming options

--- nixos/_mixins/desktop/apps/steam/default.nix
+++ nixos/_mixins/desktop/apps/steam/default.nix
@@ -9,10 +9,30 @@ lib.mkIf
   (noughtyLib.isHost [
     "skrye"
     "zannah"
+    "kratos"
+    "mercury"
   ])
   {
     # Only include mangohud if Steam is enabled
-    environment.systemPackages = with pkgs; lib.mkIf config.programs.steam.enable [ mangohud ];
+    environment.systemPackages =
+      with pkgs;
+      lib.mkIf config.programs.steam.enable [
+        mangohud
+        dotnet-sdk_8 # dotnet-sdk_8 for tmodloader
+        prismlauncher
+        (lutris.override {
+          extraLibraries = _p: [
+            # extra libraries here
+          ];
+          extraPkgs = p: [
+            p.wineWow64Packages.stable
+            p.winetricks
+            p.pixman
+            p.libjpeg
+            p.zenity
+          ];
+        })
+      ];
     # https://nixos.wiki/wiki/Steam
     programs = {
       steam = {
