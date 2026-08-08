2677df618e4426e79faaf3d12fc21d465943b1a1
Nate Felt <nate@neetfelt.com>
2026-08-08 16:21:30 +0000

    chore(caddy): update hash

--- nixos/_mixins/server/caddy/default.nix
+++ nixos/_mixins/server/caddy/default.nix
@@ -39,7 +39,7 @@ lib.mkIf useCaddy {
       plugins = [
         "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
       ];
-      hash = "sha256-xMq5qWINHdVtE6ujL/HHwKOJ+KijGns6F3yYyRyCxJo=";
+      hash = "sha256-tEcC5EffDIBCCxNeSIfyexp1tifWajGyiRbrwbvqoMQ=";
     };
     virtualHosts."${host.name}.${config.noughty.network.tailNet}" = lib.mkMerge [
       # Reverse proxy syncthing; which is configured/enabled via Home Manager
