81f649e70a34a61a73913dba7f3ea3e96d11825f
Nate Felt <nate@neetfelt.com>
2026-08-12 11:47:06 -0600

    fix(openrgb): add kratos
    - include changes for rgb on memory
    
    Signed-off-by: Nate Felt <nate@neetfelt.com>

--- nixos/_mixins/hardware/rgb/default.nix
+++ nixos/_mixins/hardware/rgb/default.nix
@@ -8,10 +8,14 @@ let
   inherit (config.noughty) host;
   username = config.noughty.user.name;
   hostRGB = {
+    kratos = "amd";
     skrye = "amd";
     zannah = "amd";
   };
   ratbagMice = {
+    "kratos" = [
+      "g502"
+    ];
     "skrye" = [
       "g305"
     ];
@@ -31,6 +35,13 @@ let
   };
 in
 lib.mkIf (!host.is.iso) {
+  boot = lib.mkIf (builtins.hasAttr host.name hostRGB) {
+    # for Corsair Vengeance memory on a Gigabyte motherboard
+    kernelParams = [
+      "acpi_enforce_resources=lax"
+    ];
+  };
+  
   environment = {
     systemPackages =
       with pkgs;
@@ -42,6 +53,10 @@ lib.mkIf (!host.is.iso) {
       ];
   };
   hardware = {
+    i2c = lib.mkIf (builtins.hasAttr host.name hostRGB) {
+      enable = true;
+    };
+    
     openrazer = lib.mkIf (builtins.hasAttr host.name razerPeripherals) {
       enable = true;
       devicesOffOnScreensaver = false;
@@ -51,14 +66,26 @@ lib.mkIf (!host.is.iso) {
       users = [ "${username}" ];
     };
   };
+  
+  networking.firewall= lib.mkIf (builtins.hasAttr host.name hostRGB) {
+    allowedTCPPorts = [ 6742 ];
+  };
+  
   services = {
     ratbagd = lib.mkIf (builtins.hasAttr host.name ratbagMice) {
       enable = true;
     };
+
     hardware.openrgb = lib.mkIf (builtins.hasAttr host.name hostRGB) {
       enable = true;
       motherboard = if builtins.hasAttr host.name hostRGB then hostRGB.${host.name} else null;
       package = pkgs.openrgb-with-all-plugins;
+      server.port = 6742;
+    };
+
+    udev = lib.mkIf (builtins.hasAttr host.name hostRGB) {
+      extraRules = builtins.readFile
+        "${pkgs.openrgb}/lib/udev/rules.d/60-openrgb.rules";
     };
   };
 }
